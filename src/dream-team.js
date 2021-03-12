3265d8880e457058e97f36ba81e5dfe9437433c0
Egor <yegor.bashchinsky@lunalabs.uk>
2021-03-12 17:03:02 +0300

    feat: dream team implemented

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,11 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require( "../extensions/custom-error" );
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam( members ) {
+    if ( !Array.isArray( members ) )
+        return false;
+
+    let result = members.filter( x => typeof ( x ) === "string" ).map( x => x.trim()[ 0 ].toUpperCase() ).sort().join( "" );
+    if ( result === "" )
+        return false;
+    return result;
 };
